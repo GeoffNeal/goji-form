58986765c813f4c2c2b7ff9618a502f396efd6f7
Geoff Neal <geoffrey_neal@hotmail.com>
2023-09-11 10:22:56 +0100

    Imported FormField component from personal project

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+interface ICallback<ArgType, Ret = void> {
+  (...args: ArgType[]): Ret;
+}
+interface IArgCollector<ArgType> {
+  (...args: ArgType[]): void;
+}
+
+/**
+ * Type definition for the `callAll` util function
+ * which provides the developer with the ability to
+ * call multiple functions at the same time with the
+ * same arguments. This is useful for using multiple
+ * event handlers for the same event.
+ */
+interface ICaller {
+  <ArgType>(
+    ...fns: (ICallback<ArgType, void> | undefined)[]
+  ): IArgCollector<ArgType>;
+}
