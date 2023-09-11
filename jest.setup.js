e5ba2305b5134a03ca9a21ff9ab4ea529fa2d382
Geoff Neal <geoffrey_neal@hotmail.com>
2023-09-11 11:37:28 +0100

    Added mock response for GET request

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,26 @@ import "whatwg-fetch";
 
 // Mock a server to provide the response from /api/orders
 const server = setupServer(
+  rest.get("/api/order", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        orders: [
+          {
+            funds: { amount: 20000, currency: "EUR" },
+            date: "2023-09-28",
+            name: "Testy McTestington",
+            shareClass: "ACME Partners Private Fund",
+          },
+          {
+            funds: { amount: 10000, currency: "EUR" },
+            date: "2023-09-19",
+            name: "Billy Bob Bobson",
+            shareClass: "Goji Private Fund",
+          },
+        ],
+      })
+    );
+  }),
   rest.post("/api/order", (req, res, ctx) => {
     return res(ctx.status(200));
   })
